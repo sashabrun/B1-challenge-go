2461837030a92b2ffce11143b8bba0b6715cbe20
Sasha Brun <sasha.brun@ynov.com>
2022-09-08 12:31:09 +0200

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -5,11 +5,11 @@ import (
 )
 
 func IsNegative(nb int) {
-	if nb < 0 {
-		z01.PrintRune('T')
+	if nb >= 0 {
+		z01.PrintRune('F')
 		z01.PrintRune('\n')
 	} else {
-		z01.PrintRune('F')
+		z01.PrintRune('T')
 		z01.PrintRune('\n')
 	}
 }
