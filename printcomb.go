e4c8b3382c828b72d94d61d8521a3eab35980698
Sasha Brun <sasha.brun@ynov.com>
2022-09-08 17:00:15 +0200

    printcomb

---
+++ printcomb.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for i := 48; i < 58; i++ {
+		for x := 48; x < 58; x++ {
+			for k := 48; k < 58; k++ {
+				if i < x && x < k {
+					z01.PrintRune(rune(i))
+					z01.PrintRune(rune(x))
+					z01.PrintRune(rune(k))
+					if i != 55 {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
