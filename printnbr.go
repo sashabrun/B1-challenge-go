bfd4affd6c41ed081b077c29bba968b443558754
Sasha Brun <sasha.brun@ynov.com>
2022-09-09 13:56:14 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -3,17 +3,6 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	/*if n < 0 {
-		z01.PrintRune(rune('0' + -n%10))
-		z01.PrintRune(rune('0' + (-n/10)%10))
-		z01.PrintRune(rune('0' + (-n/100)%10))
-	} else if n > 0 {
-		z01.PrintRune(rune('0' + n%10))
-		z01.PrintRune(rune('0' + (n/10)%10))
-		z01.PrintRune(rune('0' + (n/100)%10))
-	} else {
-		z01.PrintRune(rune('0'))
-	}*/
 	if n > 10 {
 		PrintNbr(n / 10)
 	} else if n < 0 {
@@ -21,5 +10,5 @@ func PrintNbr(n int) {
 		n = -n
 		PrintNbr(n / 10)
 	}
-	z01.PrintRune(rune(*/.n + 48))
+	z01.PrintRune(rune((n % 10) + 48))
 }
