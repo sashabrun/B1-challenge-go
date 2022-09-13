e2baa99c084dbdcae5ef7d43c39de8dbfc9277da
Sasha Brun <sasha.brun@ynov.com>
2022-09-13 15:29:38 +0200

    change isnegative

--- atoi.go
+++ atoi.go
@@ -2,6 +2,9 @@ package piscine
 
 func Atoi(s string) int {
 	j := 0
+	if len(s) == 1 || len(s) == 0 {
+		return 0
+	}
 	for i := 0; i < len(s); i++ {
 		if 48 <= int(s[i]) && int(s[i]) <= 57 {
 			j = j*10 + (int(s[i]) - 48)
@@ -13,7 +16,7 @@ func Atoi(s string) int {
 			return 0
 		}
 	}
-	if int(s[0]) == 45 {
+	if int(s[0]) == 45 || int(s[1]) == 45 {
 		j = -j
 	}
 	return j
