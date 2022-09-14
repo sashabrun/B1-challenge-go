f5d9b38022f030943db3967a3e73df350f3177d5
Sasha Brun <sasha.brun@ynov.com>
2022-09-14 10:02:43 +0200

    change isnegative

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -3,6 +3,8 @@ package piscine
 func RecursiveFactorial(nb int) int {
 	if nb == 1 || nb == 0 {
 		return nb
+	} else if nb > 100000000 {
+		return 0
 	}
 	return nb * (RecursiveFactorial(nb - 1))
 }
