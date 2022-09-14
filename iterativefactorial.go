4108f73ed72f24e588f0708493baefbbecd26706
Sasha Brun <sasha.brun@ynov.com>
2022-09-14 09:21:30 +0200

    change isnegative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,10 +4,11 @@ func IterativeFactorial(nb int) int {
 	result := 1
 	for i := 1; i <= nb; i++ {
 		result = result * i
+		if nb < 0 {
+			return 0
+		} else if nb > 1000000000 {
+			return 0
+		}
 	}
-	if nb < 0 || nb > 100000000 {
-		return 0
-	} else {
-		return result
-	}
+	return result
 }
