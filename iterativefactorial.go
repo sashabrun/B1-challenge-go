3a548fdeb636d37ea432cc75684e4a7e7e77f913
Sasha Brun <sasha.brun@ynov.com>
2022-09-14 09:30:43 +0200

    change isnegative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,13 +2,13 @@ package piscine
 
 func IterativeFactorial(nb int) int {
 	result := 1
+	if nb < 0 {
+		return 0
+	} else if nb > 1000000000 {
+		return 0
+	}
 	for i := 1; i <= nb; i++ {
 		result = result * i
-		if nb < 0 {
-			return 0
-		} else if nb > 1000000000 {
-			return 0
-		}
 	}
 	return result
 }
