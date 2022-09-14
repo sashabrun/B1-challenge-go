f2f055858f0622f5107ecbd07c91f4d9cd1caf39
Sasha Brun <sasha.brun@ynov.com>
2022-09-14 11:39:02 +0200

    change isnegative

--- iterativepower.go
+++ iterativepower.go
@@ -4,7 +4,7 @@ func IterativePower(nb int, power int) int {
 	result := 1
 	if power > 0 {
 		result = nb * IterativePower(nb, power-1)
-	} else {
+	} else if power < 0 {
 		return 0
 	}
 	return result
