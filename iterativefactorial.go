859bb09db7e1ed23a60096d8a4c65b651e6da6cc
Sasha Brun <sasha.brun@ynov.com>
2022-09-14 09:09:02 +0200

    change isnegative

---
+++ iterativefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	result := 1
+	for i := 1; i <= nb; i++ {
+		result = result * i
+	}
+	if nb < 0 {
+		return 0
+	} else {
+		return result
+	}
+}
