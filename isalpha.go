5458839e708dc0be1b729b86667edd71c96d51a6
bruns <sasha.brun@ynov.com>
2022-09-16 13:44:59 +0200

    change isnegative

--- isalpha.go
+++ isalpha.go
@@ -1,10 +1,13 @@
 package piscine
 
 func IsAlpha(s string) bool {
+	w := true
 	for i := 0; i < len(s); i++ {
-		if (s[i] >= 'A' || s[i] <= 'Z') || (s[i] >= '0' || s[i] <= '9') || (s[i] >= 'a' || s[i] <= 'z') {
-			return true
+		if (s[i] < 'A' || s[i] > 'Z') && (s[i] < '0' || s[i] > '9') && (s[i] < 'a' || s[i] > 'z') {
+			w = false
+		} else {
+			w = true
 		}
 	}
-	return false
+	return w
 }
