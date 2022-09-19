e417a4519d83b34166c46dfab11f2ea4d12705be
bruns <sasha.brun@ynov.com>
2022-09-19 09:00:00 +0200

    change isnegative

--- trimatoi.go
+++ trimatoi.go
@@ -4,9 +4,6 @@ func TrimAtoi(s string) int {
 	j := 0
 	neg := false
 	signe := true
-	if len(s) == 1 || len(s) == 0 {
-		return 0
-	}
 	for i := 0; i < len(s); i++ {
 		if 48 <= int(s[i]) && int(s[i]) <= 57 {
 			j = j*10 + (int(s[i]) - 48)
