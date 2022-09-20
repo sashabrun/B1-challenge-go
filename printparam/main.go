cfcca5df0145cc6b7f1a2645d458725eb31f8ad3
bruns <sasha.brun@ynov.com>
2022-09-20 11:28:42 +0200

    change isnegative

--- printparam/main.go
+++ printparam/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"os"
-
 	"github.com/01-edu/z01"
+	"os"
 )
 
 func main() {
-	for _, char := range os.Args[1:] {
-		for i := 0; i < len(char); i++ {
-			z01.PrintRune(rune(char[i]))
-			z01.PrintRune('\n')
+	if len(os.Args) > 0 {
+		for _, char := range os.Args[1:] {
+			for i := 0; i < len(char); i++ {
+				z01.PrintRune(rune(char[i]))
+				z01.PrintRune('\n')
+			}
 		}
 	}
 }
