5d9e1776b4251fbc75aedf07bff90415d34e2e75
bruns <sasha.brun@ynov.com>
2022-09-20 11:47:54 +0200

    change isnegative

--- revparams/main.go
+++ revparams/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	if len(os.Args) > 0 {
 		for _, char := range os.Args[1:] {
-			for i := 0; i < len(char); i++ {
+			for i := len(char) - 1; i >= 0; i-- {
 				z01.PrintRune(rune(char[i]))
 			}
 			z01.PrintRune('\n')
