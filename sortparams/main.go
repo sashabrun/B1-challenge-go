dc1403cdc0b864403c92e44e43848030342c9601
bruns <sasha.brun@ynov.com>
2022-09-20 16:25:37 +0200

    change isnegative

--- sortparams/main.go
+++ sortparams/main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
+	"github.com/01-edu/z01"
 	"os"
 )
 
 func main() {
 	cassecouille := os.Args[1:]
-	n := len(cassecouille)
-	b := false
-	for i := n - 1; i > 0; i-- {
+	for i := len(cassecouille) - 1; i >= 0; i-- {
 		for j := 0; j < i; j++ {
 			if cassecouille[j] > cassecouille[j+1] {
 				cassecouille[j], cassecouille[j+1] = cassecouille[j+1], cassecouille[j]
-				b = true
 			}
 		}
-		if !b {
-			print(cassecouille[i])
-		}
+	}
+	for _, nom := range cassecouille {
+		print(nom)
+		z01.PrintRune('\n')
 	}
 }
