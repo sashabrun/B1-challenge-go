7344e79ea1c3a6dd22225bcaba07e75579f1dae0
bruns <sasha.brun@ynov.com>
2022-09-23 10:20:36 +0200

    change isnegative

--- concatparams.go
+++ concatparams.go
@@ -3,7 +3,7 @@ package piscine
 func ConcatParams(args []string) string {
 	chainedecharactere := ""
 	for i := 0; i < len(args); i++ {
-		chainedecharactere = chainedecharactere + args[i] + "/n"
+		chainedecharactere = chainedecharactere + args[i] + "\n"
 	}
 	return chainedecharactere
 }
