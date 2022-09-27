04521baf45f376218234c573e1775a2f97903c3b
bruns <sasha.brun@ynov.com>
2022-09-27 09:36:54 +0200

    change isnegative

---
+++ point/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+//type point struct {
+//x := 42
+//	y := 21
+//}
+
+//func main() {
+//points := point{}
+
+//	points.x = x
+//	points.y = y
+
+//		fmt.Printf("points.x, points.y")
+//	}
+type point struct {
+	x rune
+	y rune
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune((points.x / 10) + 48)
+	z01.PrintRune((points.x % 10) + 48)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune((points.y / 10) + 48)
+	z01.PrintRune((points.y % 10) + 48)
+	z01.PrintRune('\n')
+}
